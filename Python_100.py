17ce64e188c2c78859df5cae01625d6cf0761a6d
flipped*zq <32381162+Jaconhunt@users.noreply.github.com>
2019-09-09 09:38:47 +0800

    Update Python_100.py

--- Python_100.py
+++ Python_100.py
@@ -22,3 +22,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+#2. 
+
+#3. 完全平方数
+import math
+
+def sqrt_num():
+    for i in range(100):
+        x = int(math.sqrt(i + 100))
+        y = int(math.sqrt(i + 268))
+        if (x**2 == i + 100) and (y**2 == i + 268):
+            print(i)
+
+sqrt_num()
